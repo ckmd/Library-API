f6cd46f05c38aad58c928a3c075e100d3d3a2b48
ckmd <rachmad.eepis@gmail.com>
2019-03-06 09:50:14 +0700

    change the port variable and procfile

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser';
 import routes from './server/routes';
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express() // setup express application
 const server = http.createServer(app);
 
